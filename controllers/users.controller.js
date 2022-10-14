814efdf63c64ae73b027ba48b9d53b115c47cd29
Tashfia Islam <tashfia.cse@gmail.com>
2022-10-14 08:40:37 -0700

    delete user from json file using its id

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,18 +2,18 @@ const users=require("../user.json");
 
 module.exports.getAllUsers=(req,res,next)=>{
     res.json(users); 
-}
+},
 
 module.exports.getRandomUsers=(req,res,next)=>{
      res.send(users[Math.floor(Math.random()*users.length)]);
-}    
+};
     
 
 module.exports.saveUsers=(req,res,next)=>{
     console.log(req.body);
     users.push(req.body);
     res.send(users);
-}
+};
 
 module.exports.updateUsers=(req,res,next)=>{
     const {id}=req.params;
@@ -23,4 +23,11 @@ module.exports.updateUsers=(req,res,next)=>{
     newUser.id=id;
     newUser.name=req.body.name;
     res.send(newUser);
+};
+
+module.exports.deleteUsers=(req,res,next)=>{
+    const {id}=req.params;
+    const filter={_id:id};
+    res.send(users.filter(user=>user.id!==Number(id)));
+
 }
--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -65,6 +65,25 @@ router
 
 router
 .route("/update/:id")
+ /**
+   * @api {patch} /users update a user 
+   * @apiDescription update all the users
+   * @apiPermission admin
+   *
+   * @apiHeader {String} Authorization   User's access token
+   *
+   * @apiParam  {Number{1-}}         [page=1]     List page
+   * @apiParam  {Number{1-100}}      [limit=10]  Users per page
+   *
+   * @apiSuccess {Object[]} all the tools.
+   *
+   * @apiError (Unauthorized 401)  Unauthorized  Only authenticated users can access the data
+   * @apiError (Forbidden 403)     Forbidden     Only admins can access the data
+   */
 .patch(usersControllers.updateUsers);
+
+router
+.route("/delete/:id")
+.delete(usersControllers.deleteUsers);
 
  module.exports=router;
